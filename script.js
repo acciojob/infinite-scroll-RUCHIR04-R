80a044bc770496c5737dc59abf7e0f9ae38b705a
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-21 10:12:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here!
-
+let itemCount = 1; 
+function addItemsToList(count) {
+    const list = document.getElementById('infi-list');
+    for (let i = 0; i < count; i++) {
+        const li = document.createElement('li');
+        li.textContent = `Item ${itemCount}`; 
+        itemCount++; 
+        list.appendChild(li); 
+    }
+}
+addItemsToList(10); 
+function checkScroll() {
+    const list = document.getElementById('infi-list');
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+        addItemsToList(2); 
+    }
+}
+document.getElementById('infi-list').addEventListener('scroll', checkScroll);
